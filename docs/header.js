af843fba7f6ea7616684e7fea88a8b803d001e14
Shunsuke Kogure <48507485+590shun@users.noreply.github.com>
2021-06-24 15:58:57 +0900

    Update header.js

--- docs/header.js
+++ docs/header.js
@@ -1,5 +1,5 @@
 document.write('\
 <div>\
-<a href="http://xpaperchallenge.org/cv/" target="_self"><img src="./img/cc_logo_2.png" style="margin-top: 5px; width:100%; height:auto; max-height:100px; max-width:525px;" /></a>\
+<a href="http://xpaperchallenge.org/cv/" target="_self"><img src="./img/cvpaper_challenge2.png" style="margin-top: 5px; width:100%; height:auto; max-height:100px; max-width:525px;" /></a>\
 </div>\
 ');
